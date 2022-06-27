dc557315116b64ef8c78acb9c00187dff630a508
bharath857 <bharath.reddy.857@gmail.com>
2022-06-27 19:28:30 +0530

    added security and separated validation errors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ const connectDB = require('./db/mongoose');
 const express = require('express');
 const app = express();
 
-
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');//Helmet helps you secure your Express apps by setting various HTTP headers.
+const cors = require('cors');//enable CORS with various options.
+const xss = require('xss-clean');//sanitize user input coming from POST body, GET queries, and url params
 
 //error handlers imports
 const notFound = require('./middleware/not-found');
@@ -15,12 +19,13 @@ const login = require('./routers/login/login');
 
 // middleware
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 // routes
 app.use('/', login);
-
-
 
 
 //middleware error handlers
@@ -37,7 +42,7 @@ const startServer = async () => {
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
-    console.log(error);
+    console.log('Error connecting to DB',error);
   }
 };
 
--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,12 +2,41 @@ const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    output: err.message || 'Something went wrong',
+    error: err
+  }
+
+  //optional can be removed
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res
+
+  if (err.name === 'ValidationError') {
+   /*  let outputString = ''
+    Object.values(err.errors)
+      .map((item) => {
+        console.log(item.message)
+        outputString = item.message + ', ' + outputString
+      })
+    customError.output = outputString   -------------------*/
+
+    customError.output = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.output = `Duplicate value entered for ${Object.keys(err.keyValue)} field, Please select another one`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  /* return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .send({msg:'Something went wrong', error:err})
+    .send({msg:'Something went wrong',msg:err.message, error:err}) */
+  return res.status(customError.statusCode).json({ output: customError.output, error: customError.error })
 }
 
 module.exports = errorHandlerMiddleware
